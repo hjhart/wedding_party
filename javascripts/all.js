8e883a7ce36e94f1a665ee3fed8c68696d8e3d9d
James Hart <hjhart@gmail.com>
2015-05-10 11:02:25 -0700

    Site updated to f764488

--- javascripts/all.js
+++ javascripts/all.js
@@ -13636,15 +13636,18 @@ var _=n.LineUtil._sqClosestPointOnSegment(t,e,i,!0);o>_&&(o=_,a=n.LineUtil._sqCl
 $(document).foundation();
 $(document).ready(function () {
 	if (Modernizr.mq('only all and (max-width: 480px)')) {
-	var map = L.map('map').setView([47.610278,-122.334872], 13);
+	var map = L.map('map').setView([47.630186, -122.391509], 13);
 	} else {
-	var map = L.map('map').setView([47.610278,-122.334872], 15);
+	var map = L.map('map').setView([47.630186, -122.391509], 15);
 	}
-
-	var openStreetMaps = new L.TileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/{type}/{z}/{x}/{y}.png', {
-	subdomains:'1234',
-	type:'osm'
-	});
+	
+	var openStreetMaps = new L.TileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	subdomains: 'abcd',
+	minZoom: 1,
+	maxZoom: 16,
+	ext: 'png'
+});
 	map.addLayer(openStreetMaps);
 	map.scrollWheelZoom.disable();
 
@@ -13656,7 +13659,7 @@ $(document).ready(function () {
 		popupAnchor:[0, -50]
 	});
 
-	var marker = L.marker([47.610278,-122.33487], { icon:myIcon })
-	marker.bindPopup("<strong>Palomino - Seattle</strong><br/><span>Get <a target='_blank' href='https://www.google.com/maps/dir//Palomino+-+Seattle,+1420+5th+Ave,+Seattle,+WA+98101,+United+States/@47.6102788,-122.3348773,13z/data=!4m12!1m3!3m2!1s0x0000000000000000:0x5514c59a73b099b9!2sPalomino+-+Seattle!4m7!1m0!1m5!1m1!1s0x54906ab48bb5b5dd:0x5514c59a73b099b9!2m2!1d-122.334872!2d47.610278'>directions</a></span>");
+	var marker = L.marker([47.630186, -122.391509], { icon:myIcon })
+	marker.bindPopup("<strong>Palisade - Seattle</strong><br/><span>Get <a target='_blank' href='https://www.google.com/maps/dir//Palisade,+Elliott+Bay+Marina,+2601+West+Marina+Place,+Seattle,+WA+98199,+United+States/@47.630287,-122.391466,15z/data=!4m12!1m3!3m2!1s0x0:0x5c2caffa98e9cb76!2sPalisade!4m7!1m0!1m5!1m1!1s0x54903fd62b5e7f83:0x5c2caffa98e9cb76!2m2!1d-122.391466!2d47.630287'>directions</a></span>");
 	marker.addTo(map);
 });
